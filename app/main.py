286aa99cfea65569f9ec55ad6b2fb6ad5b4cef93
Impu27 <impudayanand2002@gmail.com>
2025-10-03 15:17:25 +0530

    Code added to respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -5,11 +5,16 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
+    
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
+
+    # Keep responding as long as the client keeps sending data
+    while True:
+        data = connection.recv(1024)  # read up to 1024 bytes
+        if not data:  # client closed connection
+            break
+        connection.sendall(b"+PONG\r\n")  # Respond to any input with "+PONG\r\n"
 
 
 if __name__ == "__main__":
