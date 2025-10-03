24620395387f3b954ada317d0e791cc9744e69c1
Impu27 <impudayanand2002@gmail.com>
2025-10-03 18:40:40 +0530

    Code added to respond to multiple clients

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 import socket  # noqa: F401
+import threading
 
+
+def handle_client(connection):
+    #Handling all commands from a single client
+    while True:
+        data = connection.recv(1024)  # read up to 1024 bytes
+        if not data:  # client closed connection
+            break
+        connection.sendall(b"+PONG\r\n")  # Respond to any input with "+PONG\r\n"
+    connection.close()
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,14 +17,13 @@ def main():
 
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
-
-    # Keep responding as long as the client keeps sending data
-    while True:
-        data = connection.recv(1024)  # read up to 1024 bytes
-        if not data:  # client closed connection
-            break
-        connection.sendall(b"+PONG\r\n")  # Respond to any input with "+PONG\r\n"
+    while True():
+        connection, _ = server_socket.accept() #Accepting a new client
+        #Start a new thread to handle this client
+        client_thread = threading.Thread(target=handle_client, args=(connection,))
+        client_thread.start()
+    
+    
 
 
 if __name__ == "__main__":
